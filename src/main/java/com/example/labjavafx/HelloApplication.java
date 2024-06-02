0c2974978046dc1b48e7ec3c825a7d5dbd9f3ddc
Edgar Solano Rojas <esolano@proveedor.intercorp.com.pe>
2024-06-01 20:44:38 -0500

    FEAT: Refactor

--- src/main/java/com/example/labjavafx/HelloApplication.java
+++ src/main/java/com/example/labjavafx/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/layout/root.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setResizable(true);
         Image image = new Image(getClass().getResourceAsStream("/icons/retro-tv.ico"));
         stage.getIcons().add(image);
         stage.setTitle("Hands-On Tools");
